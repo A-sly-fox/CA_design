19fd0d2f5c8b4c346a89d6f43c617a2887e4b851
A-sly-fox <1291371@qq.com>
2021-10-16 19:52:09 +0800

    完成lab8除了syscall和eretn部分

---
+++ lab8_myCPU/mem_stage.v
@@ -0,0 +1,116 @@
+`include "mycpu.h"
+
+module mem_stage(
+    input                          clk           ,
+    input                          reset         ,
+    //allowin
+    input                          ws_allowin    ,
+    output                         ms_allowin    ,
+    //from es
+    input                          es_to_ms_valid,
+    input  [`ES_TO_MS_BUS_WD -1:0] es_to_ms_bus  ,
+    //to ws
+    output                         ms_to_ws_valid,
+    output [`MS_TO_WS_BUS_WD -1:0] ms_to_ws_bus  ,
+    //from data-sram
+    input  [31                 :0] data_sram_rdata,
+    
+    output [38                 :0] ms_forward,
+
+    input [`CSR_BUS_WD -1:0] es_to_ms_csr_bus,
+    output [`CSR_BUS_WD -1:0] ms_to_ws_csr_bus
+);
+
+/*
+*
+*
+*
+**
+*
+*/
+reg [`CSR_BUS_WD -1:0] es_to_ms_csr_bus_r;
+always @(posedge clk) begin
+    es_to_ms_csr_bus_r <= es_to_ms_csr_bus;
+end
+assign ms_to_ws_csr_bus = es_to_ms_csr_bus_r;
+
+reg         ms_valid;
+wire        ms_ready_go;
+
+reg [`ES_TO_MS_BUS_WD -1:0] es_to_ms_bus_r;
+wire [ 4:0] ms_res_from_mem;
+wire        ms_gr_we;
+wire [ 4:0] ms_dest;
+wire [31:0] ms_alu_result;
+wire [31:0] ms_pc;
+assign {ms_res_from_mem,  //74:70
+        ms_gr_we       ,  //69:69
+        ms_dest        ,  //68:64
+        ms_alu_result  ,  //63:32
+        ms_pc             //31:0
+       } = es_to_ms_bus_r;
+
+wire [31:0] mem_result_w;
+wire [31:0] mem_result_b;
+wire [31:0] mem_result_bu;
+wire [31:0] mem_result_h;
+wire [31:0] mem_result_hu;
+wire [31:0] ms_final_result;
+
+assign ms_to_ws_bus = {ms_gr_we       ,  //69:69
+                       ms_dest        ,  //68:64
+                       ms_final_result,  //63:32
+                       ms_pc             //31:0
+                      };
+
+assign ms_ready_go    = 1'b1;
+assign ms_allowin     = !ms_valid || ms_ready_go && ws_allowin;
+assign ms_to_ws_valid = ms_valid && ms_ready_go;
+always @(posedge clk) begin
+    if (reset) begin
+        ms_valid <= 1'b0;
+    end
+    else if (ms_allowin) begin
+        ms_valid <= es_to_ms_valid;
+    end
+
+    if (es_to_ms_valid && ms_allowin) begin
+        es_to_ms_bus_r  <= es_to_ms_bus;
+    end
+end
+
+assign ms_addr_last_00 = (ms_alu_result[1:0] == 2'b00);
+assign ms_addr_last_01 = (ms_alu_result[1:0] == 2'b01);
+assign ms_addr_last_10 = (ms_alu_result[1:0] == 2'b10);
+assign ms_addr_last_11 = (ms_alu_result[1:0] == 2'b11);
+assign mem_result_w = data_sram_rdata;
+assign mem_result_h = ms_addr_last_00 ? {{17{data_sram_rdata[15]}}, data_sram_rdata[14:0]} :
+                                     {{17{data_sram_rdata[31]}}, data_sram_rdata[30:16]};
+assign mem_result_hu = ms_addr_last_00 ? {16'b0, data_sram_rdata[15:0]} :
+                                      {16'b0, data_sram_rdata[31:16]};
+assign mem_result_b = ms_addr_last_00 ? {{25{data_sram_rdata[7]}}, data_sram_rdata[6:0]} :
+                      ms_addr_last_01 ? {{25{data_sram_rdata[15]}}, data_sram_rdata[14:8]} :
+                      ms_addr_last_10 ? {{25{data_sram_rdata[23]}}, data_sram_rdata[22:16]} :
+                                     {{25{data_sram_rdata[31]}}, data_sram_rdata[30:24]};
+assign mem_result_bu = ms_addr_last_00 ? {24'b0, data_sram_rdata[7:0]} :
+                       ms_addr_last_01 ? {24'b0, data_sram_rdata[15:8]} :
+                       ms_addr_last_10 ? {24'b0, data_sram_rdata[23:16]} :
+                                      {24'b0, data_sram_rdata[31:24]};
+
+//assign load_op = {inst_ld_w, inst_ld_b, inst_ld_bu, inst_ld_h, inst_ld_hu};
+
+assign ms_final_result = ms_res_from_mem[4] ? mem_result_w  :
+                         ms_res_from_mem[3] ? mem_result_b  :
+                         ms_res_from_mem[2] ? mem_result_bu :
+                         ms_res_from_mem[1] ? mem_result_h  :
+                         ms_res_from_mem[0] ? mem_result_hu :
+                                               ms_alu_result;
+                                         
+
+assign ms_forward =    {ms_valid        ,  //38:38
+                        ms_gr_we        ,  //37:37
+                        ms_dest         ,  //36:32
+                        ms_final_result     //31:0
+                        };
+
+endmodule
---
+++ lab8_myCPU/mycpu.h
@@ -0,0 +1,49 @@
+`ifndef MYCPU_H
+    `define MYCPU_H
+
+    `define BR_BUS_WD       33
+    `define FS_TO_DS_BUS_WD 64
+    `define DS_TO_ES_BUS_WD 156
+    `define ES_TO_MS_BUS_WD 75
+    `define MS_TO_WS_BUS_WD 70
+    `define WS_TO_RF_BUS_WD 38
+    `define DS_TO_MD_BUS_WD 71
+    `define CSR_BUS_WD 75
+
+    `define CSR_CRMD        9'h0
+    `define CSR_PRMD        9'h1
+    `define CSR_ECFG        9'h4
+    `define CSR_ESTAT       9'h5
+    `define CSR_ERA         9'h6
+    `define CSR_EENTRY      9'hc
+    `define CSR_SAVE0       9'h30
+    `define CSR_SAVE1       9'h31
+    `define CSR_SAVE2       9'h32
+    `define CSR_SAVE3       9'h33
+    `define CSR_TID         9'h40
+    `define CSR_TCFG        9'h41
+    `define CSR_TICLR       9'h44
+
+    `define CSR_CRMD_PLV        1:0
+    `define CSR_CRMD_IE         2
+    `define CSR_PRMD_PPLV       1:0
+    `define CSR_PRMD_PIE        2
+    `define CSR_ECFG_LIE        12:0
+    `define CSR_ESTAT_IS10      1:0
+    `define CSR_TICLR_CLR       0
+    `define CSR_ERA_PC          31:0
+    `define CSR_EENTRY_VA       31:6
+    `define CSR_SAVE_DATA       31:0
+    `define CSR_TID_TID         31:0
+    `define CSR_TCFG_EN         0
+    `define CSR_TCFG_PERIOD     1
+    `define CSR_TCFG_INITVAL    31:2
+
+    `define ECODE_ADE           6'h8
+    `define ECODE_ALE           6'h9
+    `define ECODE_TLBR          6'h3f
+    `define ESUBCODE_ADEF       9'h0
+    `define ESUBCODE_ADEM       9'h1
+    
+    
+`endif
